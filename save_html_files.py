a6d5be3901a1db09918945672939536623fb13fe
Jacob Baum <63393470+JacobLBaum@users.noreply.github.com>
2021-08-02 09:58:40 -0700

    Delete save_html_files.py

--- save_html_files.py
+++
@@ -1,9 +0,0 @@
-import requests
-from scraper import fetch
-
-url = "https://streeteasy.com/building/444-east-78-street-new_york/2?featured=1"
-response = fetch(url)
-content = response.content
-r = requests.get(url)
-with open('available_listing3.txt', 'w', encoding="utf-8") as file:
-    file.write(content.decode("utf-8"))
